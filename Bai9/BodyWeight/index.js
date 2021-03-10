20f61df9190e94a8732f4f19463f886e228fd86c
Just625 <bloodseekerofdota@gmail.com>
2021-03-10 09:11:38 +0700

    Tinh BMI

---
+++ Bai9/BodyWeight/index.js
@@ -0,0 +1,15 @@
+let bodyWeight = +(prompt("Enter your weight in kg: "));
+let height = +prompt("Enter your height in m: ");
+let bmi = bodyWeight/(height * height);
+if (bmi < 18.5){
+    alert("Underweight");
+} else if (bmi <25.0){
+    alert("Normal");
+} else if (bmi <30.0){
+    alert("Overweight");
+} else{
+    alert("Obese");
+}
+console.log(bodyWeight);
+console.log(height);
+console.log(bmi);
