9105921b6433cc588137117d923c63b0d6cfb9c1
Just625 <bloodseekerofdota@gmail.com>
2021-03-15 11:35:27 +0700

    Tim max trong mang(bo sung vi tri)

--- Bai12/TimMax/index.js
+++ Bai12/TimMax/index.js
@@ -1,8 +1,8 @@
 let numbers =[-3, 5, 1, 3, 2, 10];
 let max = numbers[0];
-numbers.forEach(function(item){
+numbers.forEach(function(item,index){
     if(item > max){
         max = item;
     }
-    document.getElementById("showResult").innerHTML=`So lon nhat trong mang  la ${item}`;
+    document.getElementById("showResult").innerHTML=`So lon nhat trong mang  la ${item} o vi tri ${index}`;
 })
