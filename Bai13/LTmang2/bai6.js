c00b7dbc0d7d2b3b6d2456e30c58b62f1839e4aa
Just625 <bloodseekerofdota@gmail.com>
2021-03-17 10:44:24 +0700

    LT mang 2

--- Bai13/LTmang2/bai6.js
+++ Bai13/LTmang2/bai6.js
@@ -1,7 +1,14 @@
-let array = [ 'c', '-', 'c', '2', '6', '-',"5","d","-" ];
-for(let i=0;i<array.length;i++){
-    if(array[i]=="-"){
-        array[i]=="_";
+
+let inputData = prompt("Nhap vao 1 mang cac ki tu, duoc phan cach boi dau phay: ");
+inputData = inputData.split(",");
+let inputDataArray = [];
+for(let j = 0; j<inputData.length;j++){
+    inputDataArray.push(inputData[j]);
+}
+console.log(inputDataArray);
+for(let i=0;i<inputDataArray.length;i++){
+    if(inputDataArray[i] == "-"){
+        inputDataArray[i] = "_";
     }
 }
-console.log(array);
+console.log(inputDataArray);
